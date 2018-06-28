125b5c090edfb3ba4e39e5c8e49ef15e9c8ff0c7
luan <luanoliveira100@hotmail.com>
2018-06-27 21:02:01 -0300

    xccxc

---
+++ Ex_0/Ex_0/Models/Aluno.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Ex_0.Models
+{
+    public class Aluno
+    {
+        public int ID { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+        public string Curso { get; set; }
+    }
+
+
+    public class AlunoDBContext : DbContext
+    {
+        public DbSet<Aluno> Alunos { get; set; } 
+    }
+}
